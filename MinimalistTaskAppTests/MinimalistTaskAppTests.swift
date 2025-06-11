3592e9d78a4292a6c8331bd2a5a2c72a0e5cc1b5
yboady <yanice.boady@insa-lyon.fr>
2025-06-11 23:29:25 +0200

    Initial Commit

---
+++ MinimalistTaskAppTests/MinimalistTaskAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalistTaskAppTests.swift
+//  MinimalistTaskAppTests
+//
+//  Created by Yanice Boady on 11/06/2025.
+//
+
+import Testing
+@testable import MinimalistTaskApp
+
+struct MinimalistTaskAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
